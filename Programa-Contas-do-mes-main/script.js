64dd6bc36875d0b38334ba69274f4baa3b50a55f
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-06-28 23:54:14 -0300

    Programa Contas

---
+++ Programa-Contas-do-mes-main/script.js
@@ -0,0 +1,33 @@
+const resultado = document.querySelector(".resultados");
+function removerClasse() {
+  resultado.classList.remove("resultados");
+}
+
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#resultado");
+const resp2 = document.querySelector("#mensagem");
+
+let resposta = "";
+let numContas = 0;
+let valTotal = 0;
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const descricao = frm.inDescricao.value;
+  const valor = Number(frm.inValor.value);
+
+  numContas++;
+
+  valTotal += valor;
+  resposta += descricao + " - R$:" + valor.toFixed(2) + "\n";
+
+  resp1.innerText = `${resposta} --------------------------------`;
+  resp2.innerText = `${numContas} Conta(s) - Total R$: ${valTotal.toFixed(2)}`;
+
+  frm.inDescricao.value = "";
+  frm.inValor.value = "";
+  frm.inDescricao.focus();
+
+  removerClasse();
+});
